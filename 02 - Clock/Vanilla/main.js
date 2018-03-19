f6595c639d504b59e10223437c4e32e416165a8d
Ruslan Latypov <rus_91.08@mail.ru>
2018-03-19 08:53:11 +0300

    02 - JS and CSS Clock Vanilla implementation

---
+++ 02 - Clock/Vanilla/main.js
@@ -0,0 +1,18 @@
+(function(){
+    const secondsHand = document.querySelector('.second-hand');
+    const minutesHand = document.querySelector('.min-hand');
+    const hoursHand = document.querySelector('.hour-hand');
+    function transformHand(handRelativeValue, hand){
+        const degrees = handRelativeValue*360 + 90;
+        hand.style.transform = `rotate(${degrees}deg)`;
+    }
+    function setTime(){
+        const date = new Date();
+        transformHand(date.getSeconds()/60, secondsHand);
+        transformHand(date.getMinutes()/60, minutesHand);
+        transformHand(date.getHours()/24, hoursHand);
+    }
+
+    setInterval(setTime, 1000);
+    setTime();
+})();
