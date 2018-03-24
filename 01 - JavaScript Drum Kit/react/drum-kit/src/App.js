45d3de70ea31757c8563e68169ff18f02773f2d2
Ruslan Latypov <rus_91.08@mail.ru>
2018-03-24 19:30:47 +0400

    Flex applied to Drum kit react version

--- 01 - JavaScript Drum Kit/react/drum-kit/src/App.js
+++ 01 - JavaScript Drum Kit/react/drum-kit/src/App.js
@@ -8,7 +8,7 @@ import boomSound from './sounds/boom.wav';;
 class App extends Component {
   constructor() {
     super();
-    let infos = Array.from(keyInfos.keys()).map(key=>[key, {
+    let infos = Array.from(keyInfos.keys()).map(key => [key, {
       sound: keyInfos.get(key),
       isPlaying: false
     }]);
@@ -16,33 +16,32 @@ class App extends Component {
       keyInfos: new Map(infos)
     };
     this.audios = {};
-   for(var k of this.state.keyInfos.keys()){
+    for (var k of this.state.keyInfos.keys()) {
       const audioSrc = require(`./sounds/${this.state.keyInfos.get(k).sound}.wav`);
-      this.audios[k]=new Audio(audioSrc);
+      this.audios[k] = new Audio(audioSrc);
     };
   }
-  componentDidMount(){
+  componentDidMount() {
     this.mainRef.focus();
   }
-  play(e){
-    if(!this.state.keyInfos.get(e.keyCode) || !this.audios[e.keyCode]||this.state.keyInfos.get(e.keyCode).isPlaying){
+  play(keyCode) {
+    if (!this.state.keyInfos.get(keyCode) || !this.audios[keyCode] || this.state.keyInfos.get(keyCode).isPlaying) {
       return;
     }
-    console.log(`play${e.keyCode}`);
-    const newMap = new Map(this.state.keyInfos);    
-    newMap.get(e.keyCode).isPlaying = true;    
+    const newMap = new Map(this.state.keyInfos);
+    newMap.get(keyCode).isPlaying = true;
     let newState = {
       ...this.state,
       keyInfos: newMap
     };
     this.setState(newState);
-    const sound = this.audios[e.keyCode];
+    const sound = this.audios[keyCode];
     sound.currentTime = 0;
     sound.play();
   }
-  onTransitionEnd(key, event){
-    console.log('end'+event.propertyName);    
-    if(!this.state.keyInfos.get(key) || !this.state.keyInfos.get(key).isPlaying){
+  onTransitionEnd(key, event) {
+    console.log('end' + event.propertyName);
+    if (!this.state.keyInfos.get(key) || !this.state.keyInfos.get(key).isPlaying) {
       return;
     }
     console.log(`end${key}`);
@@ -54,9 +53,9 @@ class App extends Component {
   }
   render() {
     const keys = Array.from(this.state.keyInfos.keys()).map((key) => {
-      return (<SoundKey onTransitionEnd={(e)=>this.onTransitionEnd(key,e)} key={key} code={key} sound={this.state.keyInfos.get(key).sound} isPlaying={this.state.keyInfos.get(key).isPlaying} />);
+      return (<SoundKey onClick={() => this.play(key)} onTransitionEnd={(e) => this.onTransitionEnd(key, e)} key={key} code={key} sound={this.state.keyInfos.get(key).sound} isPlaying={this.state.keyInfos.get(key).isPlaying} />);
     });
-    return (<main tabIndex="1" onKeyDown={(e)=>this.play(e)} ref={(ref)=>this.mainRef = ref}>
+    return (<main tabIndex="1" onKeyDown={(e) => this.play(e.keyCode)} ref={(ref) => this.mainRef = ref}>
       <div className="keys">
         {keys}
       </div>
--- 01 - JavaScript Drum Kit/react/drum-kit/src/SoundKey.js
+++ 01 - JavaScript Drum Kit/react/drum-kit/src/SoundKey.js
@@ -5,12 +5,9 @@ class SoundKey extends Component {
     render() {
         const soundPath = `./sounds/${this.props.sound}`;
         return (
-            <div>
-                <div className={"key"+(this.props.isPlaying?' playing':'')} onTransitionEnd={this.onTransitionEnd}>
-                    <kbd>{String.fromCharCode(this.props.code)}</kbd>
-                    <span className="sound">{this.props.sound}</span>
-                </div>
-                <audio data-key={this.props.code} src={soundPath}></audio>
+            <div className={"key" + (this.props.isPlaying ? ' playing' : '')} onTransitionEnd={this.onTransitionEnd} onClick={()=>this.props.onClick()}>
+                <kbd>{String.fromCharCode(this.props.code)}</kbd>
+                <span className="sound">{this.props.sound}</span>
             </div>
         );
     }
