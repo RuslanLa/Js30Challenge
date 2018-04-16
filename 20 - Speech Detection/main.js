5843280bc0af25e1027a576a2f09d92ece4f5d69
Ruslan Latypov <rus_91.08@mail.ru>
2018-04-16 09:33:39 +0300

    20 - Speech Detection

--- 20 - Speech Detection/main.js
+++ 20 - Speech Detection/main.js
@@ -1,2 +1,26 @@
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+const words = document.querySelector(".words");
+const addParagraph = () => {
+  p = document.createElement("p");
+  words.appendChild(p);
+};
+addParagraph();
+
+recognition
+  .addEventListener("result", e => {
+    console.log(e.results);
+    const transcript = Array.from(e.results).map(
+      result => result[0].transcript
+    );
+    p.textContent = transcript;
+    if (e.results[0].isFinal) {
+      addParagraph();
+    }
+  })
+  .join("");
+recognition.addEventListener("end", recognition.start());
+recognition.start();
